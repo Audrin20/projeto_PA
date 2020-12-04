2615c88c7a0c3db1139179f909750feef500563a
Audrin20 <Audrinh@gmail.com>
2020-12-04 01:46:47 -0300

    inserção de comentario no codigo

--- ssh.conf.py
+++ ssh.conf.py
@@ -9,6 +9,8 @@ class SSH:
 		self.ssh.load_system_host_keys()
 		self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 		self.ssh.connect(hostname='192.168.0.177',username='master',password='root')
+		# HOSTNAME -> máquina virtual
+		# USER -> Usuário da Máquina acessada via rede
 	
 	def exec_cmd(self, cmd):
 		stdin, stdout, stderr = self.ssh.exec_command(cmd)
